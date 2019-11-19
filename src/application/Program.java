4e506a780dd1dd32d89ab9b1fcf6a85b9ed5a3d5
Paulo Miguel <paulo.desouza@pg.inatel.br>
2019-11-19 20:10:25 -0300

    Atualização V2

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Teste Versao");
 		System.out.println("Boa Tarde");
-
+		System.out.println("Boa Noite");
 	}
-
 }
